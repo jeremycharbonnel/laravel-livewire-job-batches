a4f5e4091725ea74f5bf2cbf2d8ee1c7056adc72
jeremy-charbonnel <jeremy.charbonn3l@gmail.com>
2024-11-26 12:51:40 +0100

    Added livewire component with base view

---
+++ resources/views/livewire/manage-transfers.blade.php
@@ -0,0 +1,88 @@
+<div>
+    <table>
+        <thead>
+        <tr>
+            <th>&nbsp;</th>
+            <th>Status</th>
+            <th>Batch ID</th>
+            <th>Storage</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>√</td>
+            <td>Uploaded</td>
+            <td>d9cbb5a7-ea12-42b4-9fb3-3e5a7f10631f</td>
+            <td>2MB</td>
+        </tr>
+        <tr>
+            <td>X</td>
+            <td>Finished with errors</td>
+            <td>0d669854-fb2c-480f-ae04-8572ec695242</td>
+            <td>0MB</td>
+        </tr>
+        <tr>
+            <td>!!</td>
+            <td>Failed</td>
+            <td>e176a925-8534-446f-a1f6-3fc2e06fcb0f</td>
+            <td>0MB</td>
+        </tr>
+        <tr>
+            <td>
+                <svg
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle
+                            stroke-width="4"></circle>
+                    <path
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+            </td>
+            <td>
+                <div class="flex h-2 overflow-hidden rounded bg-gray-50">
+                    <div style="transform: scale({{ 50 / 100 }}, 1)"
+                         class="flex w-full origin-left flex-col bg-indigo-500 shadow-none transition-transform duration-200 ease-in-out"></div>
+                </div>
+            </td>
+            <td>
+                296fc64e-af31-401d-9895-3d18ce02931c
+            </td>
+            <td>
+                0MB
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
+
+    <div>
+        <h3>Create Batch</h3>
+        <p>Select the files you want to upload.</p>
+
+        <div>
+            <input id="files" name="files" type="file" multiple>
+        </div>
+
+        <div>
+            Files
+        </div>
+
+
+        <div>
+            <img src="#" alt="">
+            <img src="#" alt="">
+            <img src="#" alt="">
+        </div>
+
+        <div>
+            <button type="button">
+                Do some magic
+
+                <svg
+                        xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path>
+                </svg>
+            </button>
+        </div>
+    </div>
+</div>
